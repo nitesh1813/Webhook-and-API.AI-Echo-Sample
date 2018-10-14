319dc6f060830eb1c151f2978a5b9c6d02053a7b
Nitesh Sekhar <niteshsekhar@gmail.com>
2018-10-14 02:23:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ restService.post("/home", function(req, res) {
   return res.json({
     fulfillmentText:speech ,
     outputContexts: {
-     name:"Echo",
+     name:req.queryResult.intent.name,
      lifespanCount:4,
       parameters: {
         
